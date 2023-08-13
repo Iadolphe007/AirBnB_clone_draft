9d848029fc2c32646482a3adf1a5328633fac02b
Uchechukwu Madumere <madumereuchechukwu@gmail.com>
2023-08-13 07:40:07 -0700

    correction

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Definition of the Amenity class. """
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """ This class represent an Amenity and inherits from the BaseModel. """
-    name = ""
